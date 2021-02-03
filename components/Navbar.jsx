524d34c6f72dc24e441fb25a836e60eb85949979
Felix Pacheco <felix@servisenior.com>
2021-02-03 16:01:53 -0400

    Configuraciones iniciales

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Navbar = () => (
+    <div>
+        Bienvenidos!
+    </div>
+);
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
-const Index = () => {
-  return (
+const Index = () => (
     <div>
-      <Head>
-        <title>Felix Pacheco | Front-End Developer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <Head>
+            <title>Felix Pacheco | Front-End Developer</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
     </div>
-  )
-}
+);
 
 export default Index;
