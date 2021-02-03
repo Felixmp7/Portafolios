d48228cdc1b01fbc458b3c2b081c4d5eaea828b1
Felix Pacheco <felix@servisenior.com>
2021-02-03 17:27:39 -0400

    Importa Navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
+import NavbarContainer from '../components/NavbarContainer';
 
 const Index = () => (
-    <div>
+    <div className="bg-indigo-1000">
         <Head>
             <title>Felix Pacheco | Front-End Developer</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Navbar />
+        <NavbarContainer />
     </div>
 );
 
