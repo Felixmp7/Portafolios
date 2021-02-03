01940fa7725112c32e8a6e7eba4c6b3fef054c6f
Felix Pacheco <felix@servisenior.com>
2021-02-03 14:30:51 -0400

    limpiando proyecto

---
+++ pages/_app.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import '../tailwind.css';
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+App.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.instanceOf(Object),
+};
+
+App.defaultProps = {
+  Component: null,
+  pageProps: null,
+};
+
+export default App;
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Index = () => {
+  return (
+    <div>
+      <Head>
+        <title>Felix Pacheco | Front-End Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Index;
