5f705c11826f6655779d447e4589bbfae7f6c485
Felix Pacheco <felix@servisenior.com>
2021-02-17 17:37:00 -0400

    Comienza a implementar Boton reutilizable

---
+++ components/utils/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ text }) => (
+    <button type="button" className="w-full p-4 text-lg font-medium text-center transition-all duration-500 bg-pink-600 border-2 border-pink-900 rounded-full hover:bg-pink-700 ease focus:outline-none">
+        {text}
+    </button>
+);
+
+Button.propTypes = {
+    text: PropTypes.string,
+};
+
+Button.defaultProps = {
+    text: 'Botón',
+};
+
+export default Button;
