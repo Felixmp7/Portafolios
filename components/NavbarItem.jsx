d6ec616faba728111b50727e68ef02246563c6e2
Felix Pacheco <felix@servisenior.com>
2021-02-17 15:30:33 -0400

    Comienza a implementar componente NavbarItem

--- components/NavbarItem.jsx
+++ components/NavbarItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const NavbarItem = ({ title }) => (
     <button
         type="button"
-        className="flex items-center w-full p-5 text-lg font-medium text-indigo-900 transition duration-500 border-b-2 tablet:border-b-4 tablet:py-0 tablet:justify-center tablet:border-indigo-1000 ease tablet:hover:border-white focus:outline-none"
+        className="transition duration-500 navbarItem ease focus:outline-none hover:border-white hover:text-white"
     >
         {title}
     </button>
