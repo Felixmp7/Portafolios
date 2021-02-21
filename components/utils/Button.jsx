c978cb3e1ed8e789b367bd6c41f689813953d138
Felix Pacheco <felix@servisenior.com>
2021-02-21 11:08:45 -0400

    Ajusta estilos

--- components/utils/Button.jsx
+++ components/utils/Button.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Button = ({ text }) => (
     <button
         type="button"
-        className="w-full p-4 text-lg font-medium text-center transition-all duration-500 bg-pink-600 border-2 border-pink-900 rounded-full hover:bg-pink-700 ease focus:outline-none"
+        className="w-full px-4 py-3 text-lg font-medium text-center transition-all duration-500 bg-pink-600 border-2 border-pink-900 rounded-full hover:bg-pink-700 ease focus:outline-none active:outline-none"
     >
         {text}
     </button>
