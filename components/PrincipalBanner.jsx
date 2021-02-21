fb9c2ad814113a18df4f4257d00f0e418a98cfbe
Felix Pacheco <felix@servisenior.com>
2021-02-21 11:10:03 -0400

    Cambia nombre de clases y agrega nuevo icono

--- components/PrincipalBanner.jsx
+++ components/PrincipalBanner.jsx
@@ -8,10 +8,9 @@ const PrincipalBanner = () => (
             <div className="w-64 h-64 overflow-hidden border-4 border-gray-300 rounded-full">
                 <img
                     src="/assets/yo.jpg"
-                    alt="Banner"
+                    alt="Felix Pacheco"
                     width="100%"
                     height="100%"
-                    title="Photo by James Harrison on Unsplash"
                 />
             </div>
             <h1 className="mt-4 leading-none text-center text-7xl laptop:text-right tablet:text-10xl">
@@ -38,18 +37,19 @@ const PrincipalBanner = () => (
                 </span>
                 {' '}
                 con
-                <span className="font-semibold text-ssLightblue"> React JS.</span>
+                <span className="font-semibold text-react"> React JS.</span>
             </p>
             <div className="flex w-1/2 mx-auto my-5 text-7xl tablet:text-10xl justify-evenly">
                 <i className="fas fa-user-graduate text-ssYellow" />
                 <i className="fas fa-code text-ssGreen" />
-                <i className="fab fa-react text-ssLightblue" />
+                <i className="fab fa-react text-react" />
             </div>
             <p>
                 En mi portafolios podrás visualizar mi trayectoria profesional, educación, y algún dato interesante sobre mi, si deseas conocer más... empecemos!
             </p>
-            <div className="w-32 mx-auto mt-10">
+            <div className="flex flex-col items-center w-32 mx-auto mt-10">
                 <Button text="Comenzar" />
+                <i className="mt-4 text-pink-700 animate-pulse fas fa-angle-double-down" />
             </div>
         </div>
     </section>
