f77b16366bb3529f6e0d55996b7e0dc74cc62db6
Felix Pacheco <felix@servisenior.com>
2021-02-21 11:08:30 -0400

    Implementa TipLabel

---
+++ components/utils/TipLabel.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const TipLabel = ({ text }) => (
+    <div className="py-5 mx-auto mt-40 containerXl">
+        <span className="px-4 py-2 border border-indigo-800 rounded-full bg-primary">{text}</span>
+    </div>
+);
+
+TipLabel.propTypes = {
+    text: PropTypes.string,
+};
+
+TipLabel.defaultProps = {
+    text: 'Tip del Portafolio',
+};
+
+export default TipLabel;
