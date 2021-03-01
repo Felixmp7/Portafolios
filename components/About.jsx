fc4d487776c5513adf57472b8392012e68cdf3db
Felix Pacheco <felix@servisenior.com>
2021-03-01 09:33:19 -0400

    Implementa sección de About

---
+++ components/About.jsx
@@ -0,0 +1,22 @@
+import AboutCard from './AboutCard';
+import TitleWithLine from './utils/TitleWithLine';
+import histories from '../utils/static-data/about.json';
+
+const theme = {
+    textColor: 'text-primary',
+    borderColor: 'border-primary',
+};
+
+const About = () => (
+    <section id="about" className="w-full mx-auto mt-10 containerXl">
+        <TitleWithLine text="Acerca" colorText="de mi" theme={theme} />
+        <div className="grid grid-cols-4 gap-4 mx-auto mt-10 tabletLg:grid-cols-12">
+            {histories.map(((history) => (
+                <AboutCard key={history.id} {...history} />
+            )))}
+        </div>
+
+    </section>
+);
+
+export default About;
