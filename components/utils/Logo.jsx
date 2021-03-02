a29e8b73c81e13986a6af789497e283653bc144c
Felix Pacheco <felix@servisenior.com>
2021-03-02 17:55:09 -0400

    Implementa nuevo componente Logo

---
+++ components/utils/Logo.jsx
@@ -0,0 +1,10 @@
+const Logo = () => (
+    <div className="flex items-center">
+        <img src="/assets/logo/white.png" alt="logo" width="60px" height="60px" />
+        <h1 className="ml-2 text-xl font-light">
+            React Developer
+        </h1>
+    </div>
+);
+
+export default Logo;
