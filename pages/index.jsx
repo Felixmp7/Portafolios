97f942aae93c56a0ff096eede3c61266e56e12b1
Felix Pacheco <felix@servisenior.com>
2021-03-02 17:56:42 -0400

    Modifica title e icono

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
 import Head from 'next/head';
 import NavbarContainer from '../components/NavbarContainer';
 import PrincipalBanner from '../components/PrincipalBanner';
 import NextTip from '../components/NextTip';
+import JavascriptTip from '../components/JavascriptTip';
 import Knowledge from '../components/Knowledge';
 import Experiences from '../components/Experiences';
 import Skills from '../components/Skills';
+import About from '../components/About';
+import Contact from '../components/Contact';
 
 const Index = () => (
     <div className="relative overflow-hidden">
         <Head>
-            <title>Felix Pacheco | Front-End Developer</title>
-            <link rel="icon" href="/favicon.ico" />
+            <title>FP | React Developer</title>
+            <link rel="icon" href="/white.png" />
         </Head>
         <NavbarContainer />
         <PrincipalBanner />
         <Knowledge />
         <NextTip />
         <Experiences />
+        <JavascriptTip />
         <Skills />
+        <NextTip />
+        <About />
+        <Contact />
     </div>
 );
 
