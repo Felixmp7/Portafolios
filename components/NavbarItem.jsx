5ece4b2566dd027c98a82f8956a7cd344122e098
Felix Pacheco <felix@servisenior.com>
2021-03-02 17:58:44 -0400

    Agrega icono para cada NavbarItem en versión Móvil

--- components/NavbarItem.jsx
+++ components/NavbarItem.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Link as ScrollLink } from 'react-scroll';
 
-const NavbarItem = ({ title, link }) => (
+const NavbarItem = ({ title, link, icon }) => (
     <ScrollLink activeClass="active" to={link} spy smooth duration={800}>
-        <span className="h-full transition duration-500 cursor-pointer navbarItem ease focus:outline-none hover:text-white">{title}</span>
+        <div className="flex flex-col items-center">
+            <i aria-hidden className={`text-2xl ${icon}`} />
+            <span className="h-full transition duration-500 cursor-pointer navbarItem ease focus:outline-none hover:text-white">{title}</span>
+        </div>
     </ScrollLink>
 );
 
 NavbarItem.propTypes = {
     title: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
 };
 
 export default NavbarItem;
