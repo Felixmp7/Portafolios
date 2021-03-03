a7922f2c00decb221aa178401a8f8518db500598
Felix Pacheco <felix@servisenior.com>
2021-03-03 16:35:56 -0400

    Refactoriza NavbarMobile

--- components/NavbarMobile.jsx
+++ components/NavbarMobile.jsx
@@ -3,10 +3,8 @@ import NavbarItem from './NavbarItem';
 
 const NavbarMobile = ({ navbarItems }) => (
     <header className="fixed bottom-0 z-50 flex justify-between w-full px-4 py-2 border-t-2 border-indigo-700 mobileXs:px-2 mobileSm:px-0 bg-indigo-1000">
-        {navbarItems.map(({
-            id, title, link, icon,
-        }) => (
-            <NavbarItem key={id} title={title} link={link} icon={icon} />
+        {navbarItems.map((item) => (
+            <NavbarItem key={item.id} {...item} />
         ))}
     </header>
 );
