be4f50000dca236a872f20f19f3d1f24a20a5798
Felix Pacheco <felix@servisenior.com>
2021-03-03 16:58:56 -0400

    Implementa logica para mostrar más formación

--- components/Training.jsx
+++ components/Training.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import TitleWithLine from './utils/TitleWithLine';
 import TrainingItem from './TrainingItem';
 import trainingList from '../utils/static-data/knowledge.json';
@@ -7,15 +8,32 @@ const theme = {
     borderColor: 'border-react',
 };
 
-const Knowledge = () => (
-    <section id="knowledge" className="p-4 mx-auto mt-10 tablet:mt-32 tablet:p-0 containerXl">
-        <TitleWithLine text="Mi" colorText="formación" theme={theme} />
-        <div className="w-full my-10 tablet:grid tablet:grid-cols-3 tablet:gap-10">
-            {trainingList.map((item) => (
-                <TrainingItem key={item.id} {...item} />
-            ))}
-        </div>
-    </section>
-);
+const Training = () => {
+    const [isShowFirstData, setIsShowFirstData] = useState(true);
+    const firstData = trainingList.slice(0, 6);
+    const data = isShowFirstData ? firstData : trainingList;
 
-export default Knowledge;
+    return (
+        <section id="training" className="p-4 mx-auto mt-10 tablet:mt-32 tablet:p-0 containerXl">
+            <TitleWithLine text="Mi" colorText="formación" theme={theme} />
+            <div className="w-full my-10 tablet:grid tablet:grid-cols-3 tablet:gap-10">
+                {data.map((item) => (
+                    <TrainingItem key={item.id} {...item} />
+                ))}
+            </div>
+            {(!!isShowFirstData) && (
+                <div className="w-32 mx-auto">
+                    <button
+                        type="button"
+                        className="button primary"
+                        onClick={() => setIsShowFirstData(false)}
+                    >
+                        Ver más
+                    </button>
+                </div>
+            )}
+        </section>
+    );
+};
+
+export default Training;
