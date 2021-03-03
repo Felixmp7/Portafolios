7d98f6a2659868543af92e069175f6f09e69295d
Felix Pacheco <felix@servisenior.com>
2021-03-03 16:35:31 -0400

    Pasa propiedad isLight al NavbarItem y al Logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,21 +2,27 @@ import PropTypes from 'prop-types';
 import NavbarItem from './NavbarItem';
 import Logo from './utils/Logo';
 
-const Navbar = ({ navbarItems }) => (
-    <header className="py-2 text-white">
-        <nav className="flex items-center justify-between h-full mx-auto containerXl">
-            <Logo />
-            <div className="flex w-2/3 tabletXl:w-auto justify-evenly">
-                {navbarItems.map(({ id, title, link }) => (
-                    <NavbarItem key={id} title={title} link={link} />
-                ))}
-            </div>
-        </nav>
-    </header>
-);
+const Navbar = ({ navbarItems, isLight }) => {
+    const bg = isLight ? 'bg-white' : '';
+    const shadow = isLight ? 'shadow-simple' : '';
+
+    return (
+        <header className={`w-full z-50 transition-all duration-500 ease fixed px-8 py-2 text-white desktop:px-0 ${bg} ${shadow}`}>
+            <nav className="flex items-center justify-between h-full mx-auto containerXl">
+                <Logo isLight={isLight} />
+                <div className="flex w-2/3 tabletXl:w-auto justify-evenly">
+                    {navbarItems.map((item) => (
+                        <NavbarItem key={item.id} {...item} isLight={isLight} />
+                    ))}
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 Navbar.propTypes = {
     navbarItems: PropTypes.instanceOf(Array).isRequired,
+    isLight: PropTypes.bool.isRequired,
 };
 
 export default Navbar;
