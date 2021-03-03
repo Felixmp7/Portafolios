1b458e88a85b9943de1d4e059b6a39eed58fe93e
Felix Pacheco <felix@servisenior.com>
2021-03-03 16:33:38 -0400

    Pasa nueva prop isLight al componente Logo

--- components/utils/Logo.jsx
+++ components/utils/Logo.jsx
@@ -1,10 +1,23 @@
-const Logo = () => (
-    <div className="flex items-center">
-        <img src="/assets/logo/white.png" alt="logo" width="60px" height="60px" />
-        <h1 className="ml-2 text-xl font-light">
-            React Developer
-        </h1>
-    </div>
-);
+import PropTypes from 'prop-types';
+
+const Logo = ({ isLight }) => {
+    const textColor = isLight ? 'text-gray-800' : 'text-white';
+    return (
+        <div className="flex items-center">
+            <img src="/assets/logo/white.png" alt="logo" width="60px" height="60px" />
+            <h1 className={`ml-2 text-xl font-light ${textColor}`}>
+                React Developer
+            </h1>
+        </div>
+    );
+};
+
+Logo.propTypes = {
+    isLight: PropTypes.bool,
+};
+
+Logo.defaultProps = {
+    isLight: false,
+};
 
 export default Logo;
