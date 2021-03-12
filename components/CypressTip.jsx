069240a5f2a52ea78652665e2893fd76b63ee3c0
Felix Pacheco <felix@servisenior.com>
2021-03-12 15:49:49 -0400

    Implementa Cypress ttip

---
+++ components/CypressTip.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Tip from './utils/Tip';
+
+const theme = {
+    textColor: 'text-green-700',
+    borderColor: 'border-green-700',
+    bg: 'bg-green-700',
+};
+
+const CypressTip = () => {
+    const [isShowTip, setIsShowTip] = useState(false);
+    const opacityTransition = isShowTip ? 'opacity-1' : 'opacity-0';
+    const tipPosition = isShowTip ? 'static' : 'absolute';
+
+    return (
+        <Tip
+            title="El Portafolios"
+            theme={theme}
+            colorTitle="está"
+            isShowTip={isShowTip}
+            setIsShowTip={isShowTip ? null : () => setIsShowTip(true)}
+        >
+            <div className={`flex items-center text-xl transition-all duration-700 ease tablet:static tablet:text-3xl ${tipPosition} ${opacityTransition}`}>
+                <div className="flex flex-col items-center mr-10">
+                    <img
+                        src="/assets/icons/cypress.svg"
+                        alt="Cypress"
+                        width="80px"
+                        height="80px"
+                        className="mb-10"
+                    />
+                    <span className="pl-1 text-lg rounded-md">
+                        tests
+                        <span className="px-1 ml-1 bg-green-600 rounded">passing</span>
+                    </span>
+                </div>
+                <p>
+                    Testeado con
+                    <span className="font-bold text-orange"> Cypress</span>
+                    , un framework “todo en uno” que incluye librerías de aserciones, de mocks y pruebas e2e automáticas.
+                </p>
+            </div>
+        </Tip>
+    );
+};
+
+export default CypressTip;
