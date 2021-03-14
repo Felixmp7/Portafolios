1c27d0db2f8e82390edf061d44fa2a3c01ea677b
Felix Pacheco <felix@servisenior.com>
2021-03-14 17:05:04 -0400

    Refactoriza

--- components/utils/Logo.jsx
+++ components/utils/Logo.jsx
@@ -1,23 +1,10 @@
-import PropTypes from 'prop-types';
-
-const Logo = ({ isLight }) => {
-    const textColor = isLight ? 'text-gray-800' : 'text-white';
-    return (
-        <div className="flex items-center">
-            <img src="/assets/logo/white.png" alt="logo" width="60px" height="60px" />
-            <h1 className={`ml-2 text-xl font-light ${textColor}`}>
-                React Developer
-            </h1>
-        </div>
-    );
-};
-
-Logo.propTypes = {
-    isLight: PropTypes.bool,
-};
-
-Logo.defaultProps = {
-    isLight: false,
-};
+const Logo = () => (
+    <div className="flex items-center">
+        <img src="/assets/logo/indigo.png" alt="logo" width="60px" height="60px" />
+        <h1 className={'ml-2 text-xl font-light text-white\''}>
+            React Developer
+        </h1>
+    </div>
+);
 
 export default Logo;
