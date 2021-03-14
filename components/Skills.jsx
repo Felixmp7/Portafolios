e56e892ec78f82f5ca84be3e63f04f2911b300d8
Felix Pacheco <felix@servisenior.com>
2021-03-14 17:04:45 -0400

    Refactoriza sección de Skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,41 +9,15 @@ const theme = {
 const Skills = () => (
     <section id="skills" className="p-4 mx-auto mobileContainer tablet:p-0 containerXl">
         <TitleWithLine text="Mis" colorText="Habilidades" theme={theme} />
-        <div className="w-full mt-10 overflow-auto text-2xl border-4 border-pink-700 rounded">
-            <table className="w-full table-auto">
-                <thead>
-                    <tr className="border-b-4 border-pink-700">
-                        <th className="px-4 py-2" label="Technologies" />
-                        <th className="px-4 py-2">Principiante</th>
-                        <th className="px-4 py-2">Elemental</th>
-                        <th className="px-4 py-2">Intermedio</th>
-                        <th className="px-4 py-2">Avanzado</th>
-                        <th className="px-4 py-2">Experto</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {skills.map(({ id, checks, name }) => (
-                        <tr key={id} className="text-center transition-all duration-500 border-b-2 border-transparent ease hover:border-pink-700">
-                            <td className="h-20 px-4 py-2">{name}</td>
-                            <td className="h-20 px-4 py-2">
-                                {checks >= 1 && <i aria-hidden className="text-4xl text-pink-700 fas fa-check-circle" />}
-                            </td>
-                            <td className="h-20 px-4 py-2">
-                                {checks >= 2 && <i aria-hidden className="text-4xl text-pink-700 fas fa-check-circle" />}
-                            </td>
-                            <td className="h-20 px-4 py-2">
-                                {checks >= 3 && <i aria-hidden className="text-4xl text-pink-700 fas fa-check-circle" />}
-                            </td>
-                            <td className="h-20 px-4 py-2">
-                                {checks >= 4 && <i aria-hidden className="text-4xl text-pink-700 fas fa-check-circle" />}
-                            </td>
-                            <td className="h-20 px-4 py-2">
-                                {checks >= 5 && <i aria-hidden className="text-4xl text-pink-700 fas fa-check-circle" />}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="grid w-full grid-cols-2 gap-5 mt-10 overflow-auto text-2xl mobile:grid-cols-3 tabletLg:gap-10 tabletLg:grid-cols-4 laptopLg:grid-cols-5">
+            {skills.map(({
+                id, icon, name, textColor,
+            }) => (
+                <div key={id} className="flex flex-col items-center justify-center">
+                    <img src={icon || '/assets/skills/hooks.png'} alt={id} width="160px" height="160px" className="w-1/2 tabletLg:w-2/3" />
+                    <h3 className={`mt-4 text-lg tabletLg:text-xl font-bold ${textColor}`}>{name}</h3>
+                </div>
+            ))}
         </div>
     </section>
 );
