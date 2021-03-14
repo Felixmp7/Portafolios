7b170a91d711c1417a35f9eddf887cf586294098
Felix Pacheco <felix@servisenior.com>
2021-03-14 17:04:16 -0400

    Refactoriza y agrega atributo data-cy para tests

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,16 +3,18 @@ import NavbarItem from './NavbarItem';
 import Logo from './utils/Logo';
 
 const Navbar = ({ navbarItems, isLight }) => {
-    const bg = isLight ? 'bg-white' : '';
-    const shadow = isLight ? 'shadow-simple' : '';
+    const lightClasses = isLight ? 'bg-indigo-1000 border-indigo-700' : 'border-transparent';
 
     return (
-        <header className={`w-full z-50 transition-all duration-500 ease fixed px-8 py-2 text-white desktop:px-0 ${bg} ${shadow}`}>
+        <header
+            data-cy="navbar"
+            className={`w-full z-50 transition-all border-b-2 duration-500 ease fixed px-8 py-2 desktop:px-0 ${lightClasses}`}
+        >
             <nav className="flex items-center justify-between h-full mx-auto containerXl">
                 <Logo isLight={isLight} />
                 <div className="flex w-2/3 tabletXl:w-auto justify-evenly">
                     {navbarItems.map((item) => (
-                        <NavbarItem key={item.id} {...item} isLight={isLight} />
+                        <NavbarItem key={item.id} {...item} />
                     ))}
                 </div>
             </nav>
--- components/NavbarItem.jsx
+++ components/NavbarItem.jsx
@@ -3,18 +3,15 @@ import { Link as ScrollLink } from 'react-scroll';
 import useIsSmallScreen from '../hooks/useIsSmallScreen';
 
 const NavbarItem = ({
-    title, link, icon, isLight,
+    title, link, icon,
 }) => {
     const screenWidth = 761;
     const isSmallScreen = useIsSmallScreen(screenWidth);
     const offset = isSmallScreen ? 0 : -100;
 
-    const textColor = isLight
-        ? 'text-gray-600 hover:text-indigo-700'
-        : 'text-indigo-700 hover:text-gray-100';
     return (
-        <ScrollLink activeClass="active" to={link} spy smooth duration={800} offset={offset}>
-            <div className={`flex flex-col items-center h-full transition duration-500 cursor-pointer ease ${textColor}`}>
+        <ScrollLink data-cy={`navItem-${link}`} activeClass="active" to={link} spy smooth duration={800} offset={offset}>
+            <div className="flex flex-col items-center h-full transition duration-500 cursor-pointer ease text-indigo-700 hover:text-gray-100">
                 <span className="text-2xl tablet:hidden">
                     <i aria-hidden className={icon} />
                 </span>
@@ -30,10 +27,6 @@ NavbarItem.propTypes = {
     title: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
-    isLight: PropTypes.bool,
-};
-NavbarItem.defaultProps = {
-    isLight: false,
 };
 
 export default NavbarItem;
