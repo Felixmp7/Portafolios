e52169fc3b3b1efeecfc4db929af0d22696e4352
Felix Pacheco <felix@servisenior.com>
2021-03-20 19:12:51 -0400

    Implementa Hook para manejar opacidad al detectar elemento en Viewport

---
+++ hooks/useIsInView.js
@@ -0,0 +1,13 @@
+import useInView from 'react-cool-inview';
+import useIsSmallScreen from './useIsSmallScreen';
+
+const useIsInView = () => {
+    const isSmallScreen = useIsSmallScreen();
+    const threshold = isSmallScreen ? 0.2 : 0.45;
+    const { ref, inView } = useInView({ threshold, unobserveOnEnter: true });
+    const opacityEffect = inView ? 'transition-all duration-1000 ease opacity-1' : 'transition-all duration-1000 ease opacity-0';
+
+    return { ref, opacityEffect };
+};
+
+export default useIsInView;
