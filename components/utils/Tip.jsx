214154f3c2c5c15c7ca0e28d7e7f6abe08da85f3
Felix Pacheco <felix@servisenior.com>
2021-03-21 11:47:52 -0400

    Modifica estilos del label

--- components/utils/Tip.jsx
+++ components/utils/Tip.jsx
@@ -5,11 +5,10 @@ const Tip = ({
     theme, children, isShowTip, setIsShowTip, title, colorTitle, slug,
 }) => {
     const { ref, opacityEffect } = useIsInView();
-    const tipLabelColor = `${theme.bg} ${theme.borderColor}`;
     return (
         <div ref={ref} data-cy={slug} className={`my-16 ${opacityEffect}`}>
-            <div className="py-5 mx-auto containerXl">
-                <span className={`px-4 py-2 border rounded-full ${tipLabelColor}`}>Tip del Portafolio</span>
+            <div className="py-3 mx-auto containerXl">
+                <span className={`text-xl font-bold px-4 py-2 rounded-full ${theme.textColor}`}>Tip del Portafolio</span>
             </div>
             <div className={`w-full border-t-4 border-b-4 ${theme.borderColor} ${theme.bg} bg-opacity-25`}>
                 <div className="items-center p-5 mx-auto rounded-lg tablet:grid tablet:grid-cols-2 tablet:gap-10 containerXl">
