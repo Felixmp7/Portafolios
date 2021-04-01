c40769289101821b678fc6f47ab64c6b73cefc20
Felix Pacheco <felix@servisenior.com>
2021-04-01 13:59:05 -0400

    Add version to website

--- components/utils/Logo.jsx
+++ components/utils/Logo.jsx
@@ -1,9 +1,17 @@
+import { version } from '../../package.json';
+
 const Logo = () => (
-    <div className="flex items-center">
+    <div className="relative flex items-center">
         <img src="/assets/logo/indigo.png" alt="logo" width="60px" height="60px" />
         <h1 className={'ml-2 text-xl font-light text-white\''}>
             React Developer
         </h1>
+        <span
+            className="absolute right-0 text-sm text-indigo-500 text-opacity-75"
+            style={{ bottom: -5 }}
+        >
+            {`v-${version}`}
+        </span>
     </div>
 );
 
