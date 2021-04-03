1687cfcf27ee18d5c7738a9ff2255388f7d855ad
Felix Pacheco <felix@servisenior.com>
2021-04-03 09:43:00 -0400

    Starting to translate Training section

--- components/Training.jsx
+++ components/Training.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useTranslation } from 'next-i18next';
 import useIsInView from '../hooks/useIsInView';
 import TitleWithLine from './utils/TitleWithLine';
 import TrainingItem from './TrainingItem';
@@ -11,6 +12,8 @@ const theme = {
 
 const Training = () => {
     const [isShowFirstData, setIsShowFirstData] = useState(true);
+    const { t } = useTranslation('common');
+    const translate = t('training', { returnObjects: true });
     const { ref, opacityEffect } = useIsInView();
     const firstData = trainingList.slice(0, 6);
     const data = isShowFirstData ? firstData : trainingList;
@@ -22,7 +25,7 @@ const Training = () => {
             id="training"
             className={`mx-auto mt-10 mobileContainer tablet:mt-32 containerXl ${opacityEffect}`}
         >
-            <TitleWithLine text="Mi" colorText="formación" theme={theme} />
+            <TitleWithLine text={translate.text} colorText={translate.colorText} theme={theme} />
             <div className="w-full my-10 mobileLg:grid mobileLg:grid-cols-2 mobileLg:gap-5 laptopLg:grid-cols-3 laptopLg:gap-10">
                 {data.map((item) => (
                     <TrainingItem key={item.id} {...item} />
@@ -35,7 +38,7 @@ const Training = () => {
                         className="button pink"
                         onClick={() => setIsShowFirstData(false)}
                     >
-                        Ver más
+                        {t('showMoreButton')}
                     </button>
                 </div>
             )}
