c5107219cd0efa50b44d60f5b8d161477e67669e
Felix Pacheco <felix@servisenior.com>
2021-04-03 21:04:03 -0400

    Translate about section

--- components/About.jsx
+++ components/About.jsx
@@ -2,6 +2,7 @@ import AboutCard from './AboutCard';
 import TitleWithLine from './utils/TitleWithLine';
 import histories from './static-data/about.json';
 import useIsInView from '../hooks/useIsInView';
+import useIsEnglishLanguage from '../hooks/useIsEnglishLanguage';
 
 const theme = {
     textColor: 'text-custom-purple',
@@ -9,10 +10,16 @@ const theme = {
 };
 
 const About = () => {
+    const isEnglishLanguage = useIsEnglishLanguage();
     const { ref, opacityEffect } = useIsInView();
+
     return (
         <section ref={ref} id="about" className={`w-full mobileContainer mx-auto containerXl ${opacityEffect}`}>
-            <TitleWithLine text="Acerca" colorText="de mi" theme={theme} />
+            <TitleWithLine
+                text={isEnglishLanguage ? 'About' : 'Acerca'}
+                colorText={isEnglishLanguage ? 'me' : 'de mi'}
+                theme={theme}
+            />
             <div className="grid grid-cols-4 gap-4 mx-auto mt-10 tabletLg:grid-cols-12">
                 {histories.map(((history) => (
                     <AboutCard key={history.id} {...history} />
--- components/AboutCard.jsx
+++ components/AboutCard.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import useIsEnglishLanguage from '../hooks/useIsEnglishLanguage';
 
 const AboutCard = ({
     columnStyles,
     title,
+    titleTranslated,
     svgIcon,
     description,
+    descriptionTranslated,
 }) => {
+    const isEnglishLanguage = useIsEnglishLanguage();
     const [isHover, setIsHover] = useState(false);
     const imagePosition = isHover ? 'absolute top-0 left-0 ml-2' : 'static mx-auto';
 
@@ -25,8 +29,19 @@ const AboutCard = ({
                 style={{ filter: 'invert(100%)' }}
             />
             <div className="p-2 mt-12 text-right">
-                <h3 className="pt-2 font-bold leading-5 mobileLg:text-lg">{title}</h3>
-                {isHover && <p className="mt-2 text-sm mobileLg:text-lg">{description}</p>}
+                <h3
+                    className="pt-2 font-bold leading-5 mobileLg:text-lg"
+                >
+                    {isEnglishLanguage ? titleTranslated || title : title}
+                </h3>
+                {isHover
+                    && (
+                        <p
+                            className="mt-2 text-sm mobileLg:text-lg"
+                        >
+                            {isEnglishLanguage ? descriptionTranslated || description : description}
+                        </p>
+                    )}
             </div>
         </div>
     );
@@ -35,12 +50,16 @@ const AboutCard = ({
 AboutCard.propTypes = {
     columnStyles: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    titleTranslated: PropTypes.string,
     svgIcon: PropTypes.string.isRequired,
     description: PropTypes.string,
+    descriptionTranslated: PropTypes.string,
 };
 
 AboutCard.defaultProps = {
     description: undefined,
+    descriptionTranslated: undefined,
+    titleTranslated: undefined,
 };
 
 export default AboutCard;
